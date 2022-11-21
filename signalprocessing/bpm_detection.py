08fdb27bcad457b16d4da9eb7be4f1f9a4029548
bobni <bobby.l.ni@gmail.com>
2022-11-20 23:59:10 -0800

    works with live streaming now

--- signalprocessing/bpm_detection.py
+++ signalprocessing/bpm_detection.py
@@ -27,11 +27,54 @@ import numpy
 import pywt
 from scipy import signal
 
+import pyaudio
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 4
+
+WAVE_OUTPUT_FILENAME = "test.wav"
+
+
+def record_voice():
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("recording audio")
+    frames = []
+    # data_int = np.array(0)
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+        # print(data)
+    #     test = np.array(struct.unpack(str(2*CHUNK)+'B',data), dtype='b')[::8]+ 128
+    #     if np.size(data_int):
+    #         data_int = test          #makes data integer values
+    #     else:
+    #         np.concatenate(data_int,test)
+    # print(data_int)
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    # return b''.join(frames), RATE
+
 
 def read_wav(filename):
     # open file, get metadata for audio
     try:
-        wf = wave.open(filename, "rb")
+        wf = wave.open(WAVE_OUTPUT_FILENAME, "rb")
     except IOError as e:
         print(e)
         return
@@ -138,7 +181,11 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    record_voice()
     samps, fs = read_wav(args.filename)
+
+    # samps, fs = record_voice()
+    print(fs, samps[1])
     data = []
     correl = []
     bpm = 0
@@ -176,3 +223,6 @@ if __name__ == "__main__":
     n = range(0, len(correl))
     plt.plot(n, abs(correl))
     plt.show(block=True)
+
+
+#NOTES: super interesting, it appears that if the BPM < 110, it doubles it, otherwise it's accurate. 
